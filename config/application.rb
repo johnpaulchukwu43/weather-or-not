304ef16221a88c9b956e3a1e336906e09e1048e3
chukwujohnpaul <jchukwu@teamapt.com>
2021-11-12 16:36:13 +0100

    added missing files

--- config/application.rb
+++ config/application.rb
@@ -33,5 +33,9 @@ module WeatherOrNot
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.active_job.queue_adapter = :sidekiq
+    config.generators do |g|
+      g.orm :active_record, primary_key_type: :uuid
+    end
   end
 end
